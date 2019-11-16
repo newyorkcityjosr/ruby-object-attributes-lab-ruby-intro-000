543ceb34c3c6baf0796eaf9199c0dabdbfda509d
jose tavarez <newyorkcityjose@gmail.com>
2019-11-16 17:30:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 class Dog
 
-def name=(dogs_name)
+def name= (dogs_name)
   @name = dogs_name
 end
 def name
   @name
 end
-def breed=(dogs_breed)
+def breed= (dogs_breed)
   @breed = breed
 end
 def breed
